9d95dfb221f41786e2d35a034ca4bce762996c37
rexelion <rexelion@users.noreply.github.com>
2019-02-28 19:55:38 +0000

    add basic class for storing alchohol consumption entries

---
+++ src/DataEntry.java
@@ -0,0 +1,18 @@
+
+public class DataEntry {
+	private String date;
+	private String amount;
+	
+	public DataEntry(String date, String amount) {
+		this.date = date;
+		this.amount = amount;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public String getAmount() {
+		return amount;
+	}
+}
