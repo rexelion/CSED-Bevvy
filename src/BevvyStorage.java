3c39ce0be5fb213deda55b65c760785e20aa0a82
rexelion <rexelion@users.noreply.github.com>
2019-03-01 01:30:44 +0000

    add data entry writing method to the storage class

--- src/BevvyStorage.java
+++ src/BevvyStorage.java
@@ -11,4 +11,22 @@ public class BevvyStorage {
 		writer = null;
 		reader = null;
 	}
+	
+	public void addEntry(DataEntry newEntry) {
+		try {
+			writer = new BufferedWriter(new FileWriter(storageFile));
+			writer.write(newEntry.getDate() + ",");
+			writer.write(newEntry.getAmount() + ",\n");
+		} catch (IOException io) {
+			System.out.println("Error writing new entry");
+			System.out.println(io);
+		} finally {
+			try {
+				writer.close();
+			} catch (IOException closeError){
+				System.out.println("Error closing file stream");
+				System.out.println(closeError);
+			}
+		}
+	}
 }
