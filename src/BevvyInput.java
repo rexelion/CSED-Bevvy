7024e01c93b4489ea6e0f14c6f4f661b22e387f0
rexelion <rexelion@users.noreply.github.com>
2019-03-05 07:01:15 +0000

    add command reading method

--- src/BevvyInput.java
+++ src/BevvyInput.java
@@ -6,4 +6,15 @@ public class BevvyInput {
 	public BevvyInput() {
 		inputReader = new BufferedReader(new InputStreamReader(System.in));
 	}
+	
+	public String readCommand() {
+		String command = "";
+		try {
+			command = inputReader.readLine();
+		} catch (IOException io) {
+			System.out.println("Error reading command from the user");
+			System.out.println(io);
+		}
+		return command;
+	}
 }
