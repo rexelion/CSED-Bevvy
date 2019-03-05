66f404ce8e0f1348726d697019a67fda4d50dc56
rexelion <rexelion@users.noreply.github.com>
2019-03-05 06:29:30 +0000

    add entry reading method to the storage class

--- src/BevvyStorage.java
+++ src/BevvyStorage.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class BevvyStorage {
 	private String storageFile;
@@ -23,10 +25,40 @@ public class BevvyStorage {
 		} finally {
 			try {
 				writer.close();
-			} catch (IOException closeError){
+			} catch (IOException closeError) {
 				System.out.println("Error closing file stream");
 				System.out.println(closeError);
 			}
 		}
+	}
+	
+	private DataEntry fromString(String line) {
+		String elements[] = line.split(",");
+		DataEntry newEntry = new DataEntry(elements[0], elements[1]);
+		return newEntry;
+	}
+	
+	public List<DataEntry> readStorage() {
+		List<DataEntry> entries = new ArrayList<DataEntry>();
+		try {
+			reader = new BufferedReader(new FileReader(storageFile));
+			String entryLine = null;
+			while ((entryLine = reader.readLine()) != null) {
+				DataEntry newEntry = fromString(entryLine);
+				entries.add(newEntry);
+			}
+		} catch (IOException io) {
+			System.out.println("Error reading entries from the file");
+			System.out.println(io);
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException closeError) {
+				System.out.println("Error closing file stream");
+				System.out.println(closeError);
+			}
+		}
+		
+		return entries;
 	}
 }
