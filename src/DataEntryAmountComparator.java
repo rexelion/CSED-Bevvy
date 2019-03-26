92a51005df53ee03a0255b1c5e2d1de519f0719b
Alex4190 <Alex4190@github.com>
2019-03-26 13:10:04 +0000

    fix comparator by amount

--- src/DataEntryAmountComparator.java
+++ src/DataEntryAmountComparator.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Comparator;
 
-public class DataEntryAmountComparator {
+public class DataEntryAmountComparator implements Comparator<DataEntry> {
+    public int compare(DataEntry a, DataEntry b){
+        int amount1 = 0;
+        int amount2 = 0;
+        try {
+            amount1 = Integer.parseInt(a.getAmount());
+            amount2 = Integer.parseInt(b.getAmount());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+
+        return amount1 - amount2;
+
+    }
+
 }
