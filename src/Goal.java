4faf5b93effe7a49cb3b8360f51c65e54b3de9b0
rexelion <rexelion@users.noreply.github.com>
2019-04-03 21:15:56 +0100

    add Goal constructor that takes timestamps as strings

--- src/Goal.java
+++ src/Goal.java
@@ -1,3 +1,4 @@
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -15,6 +16,15 @@ public class Goal {
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.totalAmount = totalAmount;
+	}
+	
+	public Goal(String startDate, String startTime, String endDate, String endTime, String totalAmount) {
+		format = new SimpleDateFormat(dateTimeFormat);
+		try {
+			this.startDate = format.parse(startDate + " " + startTime);
+			this.endDate = format.parse(endDate + " " + endTime);
+		} catch (ParseException pe) {}
+		this.totalAmount = totalAmount; 
 	}
 	
 	public Date getStartDateTime() {
